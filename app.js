6c7e62115acfc26eea5d579c5229edae23a13e4a
Josh Kuhar <jdkuhar@gmail.com>
2016-07-01 08:15:13 -0400

    update

--- app.js
+++ app.js
@@ -1,39 +1,79 @@
 
 var playerTurnCounter = 1;
 
-// This function registers the click and places an x or an o
+// These variables keep track of whether or not a box was clicked
+var a1Clicked = 0;
+var a2Clicked = 0;
+var a3Clicked = 0;
+var b1Clicked = 0;
+var b2Clicked = 0;
+var b3Clicked = 0;
+var c1Clicked = 0;
+var c2Clicked = 0;
+var c3Clicked = 0; 
+
+// These function registers the click and places an x or an o
 $("#a1").click(function(){
-	xo(this);
+    if (a1Clicked === 0) {
+      xo(this);
+      a1Clicked++;
+    }
 });
+
 $("#a2").click(function(){
-	xo(this);
+    if (a2Clicked === 0) {
+      xo(this);
+      a2Clicked++;
+    }
 });
 $("#a3").click(function(){
-	xo(this);
+    if (a3Clicked === 0) {
+      xo(this);
+      a3Clicked++;
+    }
+	
 });
 $("#b1").click(function(){
-	xo(this);
+    if (b1Clicked === 0) {
+      xo(this);
+      b1Clicked++;
+    }
+	
 });
 $("#b2").click(function(){
-	xo(this);
+    if (b2Clicked === 0) {
+      xo(this);
+      b2Clicked++;
+    }
 });
 $("#b3").click(function(){
-	xo(this);
+    if (b3Clicked === 0) {
+      xo(this);
+      b3Clicked++;
+    }
 });
 $("#c1").click(function(){
-	xo(this);
+    if (c1Clicked === 0) {
+      xo(this);
+      c1Clicked++;
+    }
 });
 $("#c2").click(function(){
-	xo(this);
+    if (c2Clicked === 0) {
+      xo(this);
+      c2Clicked++;
+    }
 });
 $("#c3").click(function(){
-	xo(this);
+    if (c3Clicked === 0) {
+      xo(this);
+      c3Clicked++;
+    }
 });
 
-
-
+// This displays the x or o
 function xo(element) {
-	if (playerTurnCounter % 2 !== 0) {
+  if ( playerTurnCounter % 2 !== 0 ){
 			var x = $(".xo .x").clone();
 			$(element).empty();
 			$(element).html(x);
@@ -44,7 +84,11 @@ function xo(element) {
 			$(element).html(y);
 			playerTurnCounter++;
 		}
+    
 }
+
+
+
 
 
 
